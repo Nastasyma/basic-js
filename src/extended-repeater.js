88f00e71e8502f8eea5bd27428ca5dfd954e31c6
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2022-10-12 16:15:11 +0300

    fix: fix solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -41,13 +41,13 @@ module.exports = {
   repeater
 };
 
-console.log((repeater('la', { repeatTimes: 3 }))) //'la+la+la'
-console.log((repeater('la', { repeatTimes: 3, separator: 's', addition: '+', additionRepeatTimes: 1 }))) //'la+sla+sla+'
-console.log((repeater('LALA', { repeatTimes: 3, separator: 's', addition: '++', additionRepeatTimes: 1 }))) //'LALA++sLALA++sLALA++'
-console.log((repeater('12345', { repeatTimes: 5, separator: '3 words separator' }))) //'123453 words separator123453 words separator123453 words separator123453 words separator12345'
-console.log((repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))) //'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
-console.log((repeater('REPEATABLE_STRING', { repeatTimes: 2, separator: '222', addition: 'ADDITION', additionRepeatTimes: 3 }))) //'REPEATABLE_STRINGADDITION|ADDITION|ADDITION222REPEATABLE_STRINGADDITION|ADDITION|ADDITION'
-const objWithSpecificCoercion = {
+//console.log((repeater('la', { repeatTimes: 3 }))) //'la+la+la'
+//console.log((repeater('la', { repeatTimes: 3, separator: 's', addition: '+', additionRepeatTimes: 1 }))) //'la+sla+sla+'
+//console.log((repeater('LALA', { repeatTimes: 3, separator: 's', addition: '++', additionRepeatTimes: 1 }))) //'LALA++sLALA++sLALA++'
+//console.log((repeater('12345', { repeatTimes: 5, separator: '3 words separator' }))) //'123453 words separator123453 words separator123453 words separator123453 words separator12345'
+//console.log((repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))) //'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
+//console.log((repeater('REPEATABLE_STRING', { repeatTimes: 2, separator: '222', addition: 'ADDITION', additionRepeatTimes: 3 }))) //'REPEATABLE_STRINGADDITION|ADDITION|ADDITION222REPEATABLE_STRINGADDITION|ADDITION|ADDITION'
+/*const objWithSpecificCoercion = {
   [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
 };
-console.log((repeater(objWithSpecificCoercion, { repeatTimes: 2, addition: objWithSpecificCoercion }))) //'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT'
+console.log((repeater(objWithSpecificCoercion, { repeatTimes: 2, addition: objWithSpecificCoercion }))) //'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT'*/
